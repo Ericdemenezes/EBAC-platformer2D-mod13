29a3c861a5e686dd73d17c63122e6ddc758746a7
Ericdemenezes <ericmvieira@hotmail.com>
2023-09-01 11:42:54 -0300

    Jump physics added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,13 +6,21 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f,0);
 
     public float speed;
 
+    public float forceJump;
+
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleJump();
+        HandleMovement();
+
+    }
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
@@ -23,5 +31,24 @@ public class Player : MonoBehaviour
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
         }
+        
+        
+        if(myRigidBody.velocity.x >0)
+        {
+            myRigidBody.velocity -= friction;
+        }
+
+        else if(myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity += friction;
+        }
+     
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            myRigidBody.velocity = Vector2.up * forceJump;
     }
 }
+
